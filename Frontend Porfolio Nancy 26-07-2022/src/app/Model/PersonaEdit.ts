e9420be99e180b92e97a80517a587495f66c258e
Nancy <nan_alaobra@hotmail.com>
2022-09-26 01:42:37 -0300

    actualización frontend

--- Frontend Porfolio Nancy 26-07-2022/src/app/Model/PersonaEdit.ts
+++ Frontend Porfolio Nancy 26-07-2022/src/app/Model/PersonaEdit.ts
@@ -3,16 +3,16 @@ id?:number;
 nombre:string;
 apellido: string;
 img: string;
-portada: string;
+animacion: string;
 descripcion: string;
 ubicacion: string;
 
 constructor(nombre: string,
-    apellido:string, img:string, portada:string, descripcion:string, ubicacion:string ){
+    apellido:string, img:string, animacion:string, descripcion:string, ubicacion:string ){
     this.nombre= nombre; 
     this.apellido= apellido;
     this.img= img;
-    this.portada= portada;
+    this.animacion= animacion;
     this.descripcion = descripcion;
     this.ubicacion= ubicacion;
     }
